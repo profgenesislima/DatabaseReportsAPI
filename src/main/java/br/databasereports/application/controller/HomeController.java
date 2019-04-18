42b64cfcd73ba1e3f82fd2bc898377345c5516d3
profgenesislima <profgenesislima@gmail.com>
2019-04-18 01:09:20 -0300

    Working Candidate Architecture.

--- src/main/java/br/databasereports/application/controller/HomeController.java
+++ src/main/java/br/databasereports/application/controller/HomeController.java
@@ -1,15 +1,71 @@
 package br.databasereports.application.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-@RestController
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.databasereports.db.connection.PostgresConnection;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+
+@Controller
 public class HomeController {
 
-	@GetMapping("/")
+	@RequestMapping(path="/", method=RequestMethod.GET)
 	public String index() {	
-		System.out.println("INDEX ON CONTROLLER");	
+		System.out.println("INDEX ON CONTROLLER");
+
 	   return "index";
 	}
 	
+	
+	 @RequestMapping(value = "/report/{nome_relatorio}", method = RequestMethod.GET,
+	            produces = MediaType.APPLICATION_PDF_VALUE)
+	    public ResponseEntity<InputStreamResource> generateAndDeliverReport() throws IOException, JRException {
+
+	        
+
+		  String report = "departamento_has_professor";
+			
+			JasperReport rep = JasperCompileManager.compileReport(report+".jrxml");
+			
+			System.out.println("JASPER REPORT "+rep.getName());
+		 
+		    Map<String, Object> params = new HashMap<String, Object>();
+
+		    PostgresConnection con  = new PostgresConnection();
+			
+		    
+
+			JasperPrint jasperPrint = JasperFillManager.fillReport(rep,
+					params, con.connect());
+			
+			JasperExportManager.exportReportToPdfFile(jasperPrint,
+					report+".pdf");
+
+			
+
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.add("Content-Disposition", "inline; filename="+report+".pdf");
+
+	        return ResponseEntity
+	                .ok()
+	                .headers(headers)
+	                .contentType(MediaType.APPLICATION_PDF)
+	                .body(new InputStreamResource(new FileInputStream(report+".pdf")));
+	    }
 }
---
+++ src/test/java/br/estacio/report/tests/jasper/JasperReportsTestDrive.java
@@ -0,0 +1,39 @@
+package br.estacio.report.tests.jasper;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
+import br.databasereports.db.connection.PostgresConnection;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+
+public class JasperReportsTestDrive {
+
+	public static void main(String[] args) throws JRException {
+
+		String report = "departamento_has_professor";
+		
+		JasperReport rep = JasperCompileManager.compileReport(report+".jrxml");
+		
+		System.out.println("JASPER REPORT "+rep.getName());
+	 
+	    Map<String, Object> params = new HashMap<String, Object>();
+
+	    PostgresConnection con  = new PostgresConnection();
+		
+	    //JasperFillManager.fillReportToFile(report+".jasper", params);
+
+		JasperPrint jasperPrint = JasperFillManager.fillReport(rep,
+				params, con.connect());
+		
+		JasperExportManager.exportReportToPdfFile(jasperPrint,
+				report+".pdf");
+
+	}
+
+}
